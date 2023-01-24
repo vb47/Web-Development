d1b728f9d1f23ea5942299b21c62e77f9f97bdbd
Vaibhav <vsalur22@gmail.com>
2023-01-24 09:32:12 +0530

    Downloaded initial files of The Simon Game

---
+++ Projects/scripts/02_The_Simon_Game.js
@@ -0,0 +1,7 @@
+var greenAudio = new Audio("./Resources/02_The_Simon_Game/sounds/green.mp3");
+var redAudio = new Audio("./Resources/02_The_Simon_Game/sounds/red.mp3");
+var yellowAudio = new Audio("./Resources/02_The_Simon_Game/sounds/yellow.mp3");
+var blueAudio = new Audio("./Resources/02_The_Simon_Game/sounds/blue.mp3");
+var wrongAudio = new Audio("./Resources/02_The_Simon_Game/sounds/wrong.mp3");
+
+alert("Hello World!");
