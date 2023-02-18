f98953aaea3b33b8c2d9c89bbaa5213c250f4d37
Vaibhav <vsalur22@gmail.com>
2023-02-18 16:13:55 +0530

    Completed API Endpoints and Paths practice.

--- Practice_NodeJs/04_api_practice/server.js
+++ Practice_NodeJs/04_api_practice/server.js
@@ -1,23 +1,30 @@
 const express = require("express");
 const https = require("https");
-//var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
 // View engine setup
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
     res.write("<h1>API Practice:</h1> <br>");
     res.write("<ul>");
     res.write("<li>Understanding Enndpoints: <a href='/endpoint'>Link</a></li>");
-    res.write("<li>Understanding Paths: <a href='/paths'>Link</a></li>");
-    res.write("<li>Understanding Parameters: <a href='/parameters'>Link</a></li>");
+    res.write("<li>Understanding Paths: <a href='/paths'>Link</a></li></ul>");
     res.send();
 });
 
+/*
+ * Endpoint is the url through which we can send requests to get data.
+ * Endpoints is the simplest form of API call.
+ * 
+ * Consider an API - [Kanye Rest](https://kanye.rest/), 
+ * here the endpoint is given in website which is - [https://api.kanye.rest](https://api.kanye.rest/). 
+ * When you visit this endpoint it prints back the data it is sending.**
+ */
 app.get("/endpoint", function(req, res){
-
     const url = "https://api.kanye.rest";
     https.get(url, function(resp){
         resp.on("data", function(data){
@@ -25,7 +32,67 @@ app.get("/endpoint", function(req, res){
             //res.send("<h1>" + quote.quote + "</h1>");
             res.render("endpoint", {quoteOfTheDay: quote.quote});
         });
+    });
+});
 
+/*
+ * Paths And Parameters
+ * Paths are the extentions to the url. 
+ * If you want to get data by sending parameters specified in docs, we use paths.
+ * To send any parameters  - https://<url>/<path>?<parameter_name>=<value>
+ * 
+ * Consider the JokeAPI, that sends random jokes, 
+ * if you go to documentation, you will get url and paths through which you can post parameters.
+ * They have several categories to get a specific joke.
+ * Know more about the API - https://sv443.net/jokeapi/v2/
+ * In this type of API we use endpoints we will get error as it needs some parameters.
+ */
+app.get("/paths", function(req, res){
+    res.render("paths");
+});
+
+app.post("/joke", function(req, res){
+    var url = "https://v2.jokeapi.dev/joke/";
+    //console.log(req.body);
+    if(req.body.Any == "true")
+        url += "Any";
+    else {
+        if(req.body.Programming == "true")
+            url += "Programming,";
+        if(req.body.Misc == "true")
+            url += "Misc,";
+        if(req.body.Dark == "true")
+            url += "Dark,";
+        if(req.body.Pun == "true")
+            url += "Pun,";
+        if(req.body.Spooky == "true")
+            url += "Spooky,";
+        
+        url = url.slice(0,-1);
+    }
+
+    url += "?blacklistFlags=";
+
+    if(req.body.nsfw=="true")
+        url += "nsfw,";
+    if(req.body.religious=="true")
+        url += "religious,";
+    if(req.body.political=="true")
+        url += "political,";
+    if(req.body.racist=="true")
+        url += "racist,";
+    if(req.body.sexist=="true")
+        url += "sexist,";
+
+    url = url.slice(0,-1);
+    console.log(url);
+
+    https.get(url, function(resp){
+        resp.on("data", function(data){
+            const quote = JSON.parse(data);
+            console.log(quote);
+            res.render("joke", {joke: quote.setup, delivery: quote.delivery, onlyJoke: quote.joke});
+        });
     });
 });
 
